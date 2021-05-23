e823ccdbaf3a198798e54a6ccf0251ca65168c00
neogim <75684023+neogim@users.noreply.github.com>
2021-05-23 12:57:06 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+// /**
+//  * <ul>タグの直下に<div>タグを使用するのはアンチパターン
+//  */
+
+// <ul>
+//   <li>
+//     <div>
+//       <p>TODOです</p>
+//       <button>完了</button>
+//       <button>削除</button>
+//     </div>
+//   </li>
+// </ul>

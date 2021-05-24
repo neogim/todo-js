15f68a8bab8db4ee631eb99e2a2ad05d9a871358
neogim <75684023+neogim@users.noreply.github.com>
2021-05-24 11:33:01 +0900

    js実装中 js実装中

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").Value;
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
 
 // /**
 //  * <ul>タグの直下に<div>タグを使用するのはアンチパターン

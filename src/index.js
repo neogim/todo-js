884639382da06e0e21ac50d335f395745863a90a
neogim <75684023+neogim@users.noreply.github.com>
2021-05-25 00:10:57 +0900

    完了ボタンまで実装 完了ボタンまで実装

--- src/index.js
+++ src/index.js
@@ -9,13 +9,40 @@ const onClickAdd = () => {
   div.className = "list-row";
   //liタグ生成
   const li = document.createElement("li");
+  li.className = "list-text";
   li.innerText = inputText;
 
   //button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //押された削除ボタンの親タグ（div)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    //？？？？上で消しているのに、下でaddTargetで取れる理由がわからない。
+
+    //完了リストに追加する要素
+    //親要素を取得する
+    const addTarget = completeButton.parentNode;
+    //TODO内容テキストを取得！！！
+    const text = addTarget.firstElementChild.innerText;
+    //div以下を初期化 ←ここが良くわからない
+    addTarget.textContent = null;
+    //console.log(addTarget);
+
+    //liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+    //buttonタグ生成
+    const backbutton = document.createElement("button");
+    backbutton.innerText = "戻す";
+
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backbutton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(div);
   });
 
   //button(削除)タグ生成
@@ -23,8 +50,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     //押された削除ボタンの親タグ（div)を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   //divタグの子要素に各要素を設定
@@ -37,6 +63,11 @@ const onClickAdd = () => {
 
   console.log(li);
   // alert(inputText);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
